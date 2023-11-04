6d2ba573a5845f333bbf7cb9d9867e36e087d39f
Ulises <ulisescappa@gmail.com>
2023-11-04 12:37:24 -0300

    Add post, comment and like models tests.

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+    it 'updates post comments_counter after save' do
+      user = User.create(name: 'Alice', photo: 'https://unsplash.com/photos/AlicePhoto', bio: 'Bio for Alice')
+      post = Post.create(title: 'Test Post', text: 'This is a test post', user: user)
+      comment = Comment.create(post: post, user: user, text: 'Hi Tom!')
+  
+      post.reload
+  
+      expect(post.comments_counter).to eq(1)
+    end
+  
+    it 'belongs to a user' do
+      user = User.create(name: 'Alice', photo: 'https://unsplash.com/photos/AlicePhoto', bio: 'Bio for Alice')
+      comment = Comment.create(user: user, text: 'Comment text')
+      
+      expect(comment.user).to eq(user)
+    end
+  
+    it 'belongs to a post' do
+      user = User.create(name: 'Alice', photo: 'https://unsplash.com/photos/AlicePhoto', bio: 'Bio for Alice')
+      post = Post.create(title: 'Test Post', text: 'This is a test post', user: user)
+      comment = Comment.create(post: post, text: 'Comment text')
+      
+      expect(comment.post).to eq(post)
+    end
+  end
