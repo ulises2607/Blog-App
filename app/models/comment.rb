7d9b5c2d26501548317bc8c4a54ca7c29924bb68
Ulises <ulisescappa@gmail.com>
2023-11-04 04:04:58 -0300

    Add post, like, comment and user models. Add methods to update counters. Add methods to return 3 recents post for a given user and a method wich returns the 5 most recent comments for a given post

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+class Comment < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :post
+
+    after_save :update_comment_count
+
+    def update_comment_count
+        post.update(comments_counter: post.comments.count)
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+    has_many :posts, foreign_key: "author_id", dependent: :destroy
+    has_many :comments
+    has_many :likes
+    
+    def last_three_posts
+      posts.order(created_at: :desc).limit(3)
+    end
+  end
+  
