70f73900ee2fd1d6dc502afa8fe78179a8e07499
Ulises <ulisescappa@gmail.com>
2023-11-04 12:38:38 -0300

    Fix linters errors

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,29 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-    it 'updates post likes_counter after save' do
-      user = User.create(name: 'Alice', photo: 'https://unsplash.com/photos/AlicePhoto', bio: 'Bio for Alice')
-      post = Post.create(title: 'Test Post', text: 'This is a test post', user: user)
-      like = Like.create(post: post, user: user)
-  
-      post.reload
-  
-      expect(post.likes_counter).to eq(1)
-    end
-  
-    it 'belongs to a user' do
-      user = User.create(name: 'Alice', photo: 'https://unsplash.com/photos/AlicePhoto', bio: 'Bio for Alice')
-      like = Like.create(user: user)
-      
-      expect(like.user).to eq(user)
-    end
-  
-    it 'belongs to a post' do
-      user = User.create(name: 'Alice', photo: 'https://unsplash.com/photos/AlicePhoto', bio: 'Bio for Alice')
-      post = Post.create(title: 'Test Post', text: 'This is a test post', user: user)
-      like = Like.create(post: post)
-      
-      expect(like.post).to eq(post)
-    end
+  it 'updates post likes_counter after save' do
+    user = User.create(name: 'Alice', photo: 'https://unsplash.com/photos/AlicePhoto', bio: 'Bio for Alice')
+    post = Post.create(title: 'Test Post', text: 'This is a test post', user:)
+    Like.create(post:, user:)
+
+    post.reload
+
+    expect(post.likes_counter).to eq(1)
   end
-  
+
+  it 'belongs to a user' do
+    user = User.create(name: 'Alice', photo: 'https://unsplash.com/photos/AlicePhoto', bio: 'Bio for Alice')
+    like = Like.create(user:)
+
+    expect(like.user).to eq(user)
+  end
+
+  it 'belongs to a post' do
+    user = User.create(name: 'Alice', photo: 'https://unsplash.com/photos/AlicePhoto', bio: 'Bio for Alice')
+    post = Post.create(title: 'Test Post', text: 'This is a test post', user:)
+    like = Like.create(post:)
+
+    expect(like.post).to eq(post)
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it 'should create an user' do
-      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-      expect(user).to be_valid
-    end
+  it 'should create an user' do
+    user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    expect(user).to be_valid
+  end
 
-    describe '#last_three_posts' do
+  describe '#last_three_posts' do
     it 'returns the last three posts' do
       user = User.create(name: 'Alice', photo: 'https://unsplash.com/photos/AlicePhoto', bio: 'Bio for Alice')
-      
-      post1 = Post.create(user: user, title: 'Post 1', text: 'Content 1', created_at: 1.day.ago)
-      post2 = Post.create(user: user, title: 'Post 2', text: 'Content 2', created_at: 2.days.ago)
-      post3 = Post.create(user: user, title: 'Post 3', text: 'Content 3', created_at: 3.days.ago)
-      post4 = Post.create(user: user, title: 'Post 4', text: 'Content 4', created_at: 4.days.ago)
+
+      post1 = Post.create(user:, title: 'Post 1', text: 'Content 1', created_at: 1.day.ago)
+      post2 = Post.create(user:, title: 'Post 2', text: 'Content 2', created_at: 2.days.ago)
+      post3 = Post.create(user:, title: 'Post 3', text: 'Content 3', created_at: 3.days.ago)
+      Post.create(user:, title: 'Post 4', text: 'Content 4', created_at: 4.days.ago)
 
       last_posts = user.last_three_posts
 
@@ -24,8 +24,8 @@ RSpec.describe User, type: :model do
     it 'returns fewer than three posts if there are less than three' do
       user = User.create(name: 'Bob', photo: 'https://unsplash.com/photos/BobPhoto', bio: 'Bio for Bob')
 
-      post1 = Post.create(user: user, title: 'Post 1', text: 'Content 1', created_at: 1.day.ago)
-      post2 = Post.create(user: user, title: 'Post 2', text: 'Content 2', created_at: 2.days.ago)
+      post1 = Post.create(user:, title: 'Post 1', text: 'Content 1', created_at: 1.day.ago)
+      post2 = Post.create(user:, title: 'Post 2', text: 'Content 2', created_at: 2.days.ago)
 
       last_posts = user.last_three_posts
 
@@ -41,5 +41,4 @@ RSpec.describe User, type: :model do
       expect(last_posts).to be_empty
     end
   end
-
-  end
+end
