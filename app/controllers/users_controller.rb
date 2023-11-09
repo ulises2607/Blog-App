e8282863a4f84b11a4cd990dcc71f956be8f65b6
Ulises <ulisescappa@gmail.com>
2023-11-09 10:57:10 -0300

    Add post controller. Add post routes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,6 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find_by(id: params[:id])
+        @posts = @user.posts.order(created_at: :desc)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root 'users#index'
+  get 'users/:id', to: 'users#show', as: :user
+  get 'users', to: 'users#index', as: :users
+  get "users/:user_id/posts", to: "posts#index", as: :user_posts
+  get "users/:user_id/posts/:id", to: "posts#show", as: :user_post
+   
+
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  root 'users#index'
+  
   
   resources :users, only: [:index , :show]
 
