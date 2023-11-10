88c8d1718384e317994ad8aaa586ed30e3f9c21e
Ulises <ulisescappa@gmail.com>
2023-11-10 13:11:34 -0300

    Add post and user controllers tests in request folder

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,42 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns a succesful response' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      post = Post.create(author_id: user.id, title: 'Post 1', text: 'Content 1')
+      get user_posts_path(user.id)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'assigns @posts' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      post = Post.create(author_id: user.id, title: 'Post 1', text: 'Content 1')
+      get user_posts_path(user.id)
+      expect(assigns(:posts)).to eq([post])
+    end
   end
+
+  describe 'GET /show' do
+    it 'returns a succesful response' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      post = Post.create(author_id: user.id, title: 'Post 1', text: 'Content 1')
+      get user_post_path(user.id, post.id)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'assigns @post' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      post = Post.create(author_id: user.id, title: 'Post 1', text: 'Content 1')
+      get user_post_path(user.id, post.id)
+      expect(assigns(:post)).to eq(post)
+    end
+
+    it 'renders the show template' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      post = Post.create(author_id: user.id, title: 'Post 1', text: 'Content 1')
+      get user_post_path(user.id, post.id)
+      expect(response).to render_template(:show)
+    end
+  end
+
 end
