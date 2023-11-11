fe8f67be6023334c56c6392bcf5e67cedecc30bf
Ulises <ulisescappa@gmail.com>
2023-11-11 20:08:24 -0300

    Create comments and likes controllers. Add new post view

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'users', to: 'users#index', as: :users
   get "users/:user_id/posts", to: "posts#index", as: :user_posts
   get "users/:user_id/posts/:id", to: "posts#show", as: :user_post
+  get "users/:user_id/posts/new", to: "posts#new", as: :new_user_post
    
 
   get 'up' => 'rails/health#show', as: :rails_health_check
