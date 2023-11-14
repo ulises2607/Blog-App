efff99ad6dadc7599ce95afaadfd64ccfa00ea43
Ulises <ulisescappa@gmail.com>
2023-11-14 17:28:15 -0300

    Fix show img

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
