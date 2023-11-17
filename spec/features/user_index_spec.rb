365d7d8db6d4b8a9a834fddece7fea2a71abbc37
Ulises <ulisescappa@gmail.com>
2023-11-17 00:49:44 -0300

    Fix user integration test comments

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,5 +1,3 @@
-# spec/features/user_index_spec.rb
-
 require 'rails_helper'
 
 RSpec.feature 'User index page', type: :feature do
@@ -7,7 +5,7 @@ RSpec.feature 'User index page', type: :feature do
   scenario 'I can see the username of all other users' do
     visit users_path
 
-    # Verificar que se muestren los nombres de usuario en la página de índice de usuarios
+    # Verify that the usernames are displayed on the users index page
     expect(page).to have_content('Tom')
     expect(page).to have_content('Silvia')
   end
@@ -15,25 +13,25 @@ RSpec.feature 'User index page', type: :feature do
   scenario 'I can see the profile picture for each user' do
     visit users_path
 
-    # Verificar que se muestren las imágenes de perfil en la página de índice de usuarios
+    # Verify that the profile pictures are displayed on the users index page
     expect(page).to have_css('.img-user-container img', count: User.count)
   end
 
   scenario 'I can see the number of posts each user has written' do
     visit users_path
 
-    # Verificar que se muestra el número de posts para cada usuario en la página de índice de usuarios
-    expect(page).to have_content('Number of posts: 25') # Ajusta el número según tus datos específicos
-    expect(page).to have_content('Number of posts: 8')  # Ajusta el número según tus datos específicos
+    # Verify that the number of posts for each user is displayed on the users index page
+    expect(page).to have_content('Number of posts: 25') # Adjust the number based on your specific data
+    expect(page).to have_content('Number of posts: 8')  # Adjust the number based on your specific data
   end
 
   scenario 'When I click on a user, I am redirected to that user\'s show page' do
     visit users_path
 
-    # Hacer clic en el enlace del primer usuario
+    # Click on the link for the first user
     click_link 'Tom'
 
-    # Verificar que se redirige a la página del usuario correctamente
+    # Verify that it redirects to the user's show page correctly
     expect(page).to have_current_path(user_path(User.find_by(name: 'Tom')))
   end
 end
