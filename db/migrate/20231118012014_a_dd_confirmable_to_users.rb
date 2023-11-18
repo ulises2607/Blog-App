4aba50bb4ac7525d15d55bec52881a58172df375
Ulises <ulisescappa@gmail.com>
2023-11-18 00:49:29 -0300

    Add email confirmation columns

---
+++ db/migrate/20231118012014_a_dd_confirmable_to_users.rb
@@ -0,0 +1,16 @@
+class ADdConfirmableToUsers < ActiveRecord::Migration[7.1]
+  def self.up
+    change_table :users do |t|
+
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email 
+
+    end
+
+    add_index :users, :confirmation_token,   unique: true
+
+  end
+end
