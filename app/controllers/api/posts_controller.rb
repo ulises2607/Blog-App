1ba362ec331d09104a9d461437dd3f0e58ff3238
Cristian Villa <c.villavirgen@gmail.com>
2023-11-18 19:56:19 -0600

    Testing

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,16 @@
+module Api
+    class PostsController < ApplicationController
+      before_action :set_user, only: [:index]
+  
+      def index
+        @posts = @user.posts
+        render json: @posts
+      end
+  
+      private
+  
+      def set_user
+        @user = User.find(params[:user_id])
+      end
+    end
+  end
